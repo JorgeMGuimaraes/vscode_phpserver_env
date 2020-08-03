a379e4c61f7449b216027fa1b44220c00b87fbd6
Jorge Guimaraes <jmguimaraes@outlook.com>
2020-08-03 11:44:30 -0300

    instal extensions removed

--- install_extensions.sh
+++
@@ -1,8 +0,0 @@
-echo "Downloading extensions"
-sudo docker exec -it php-fpm docker-php-ext-install \
-    pdo \
-    pdo_mysql \
-    mysqli
-echo "Restarting..."
-sudo docker-compose restart
-echo "Conplete"
