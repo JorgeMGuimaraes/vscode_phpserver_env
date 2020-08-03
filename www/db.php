7d64664af4142de83638ff0b9bf00d44d15eff57
Jorge Guimaraes <jmguimaraes@outlook.com>
2020-08-02 23:55:40 -0300

    connecting using non root user

--- www/db.php
+++ www/db.php
@@ -1,12 +1,12 @@
 <?php
 $host = "db";
-$username = "root";
-$password = "root";
+$username = "jorge";
+$password = "hguyFt6S95dgfR4ryb";
 $db = "teste";
 try {
     $conn = new PDO("mysql:host=$host;dbname=$db", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "<h2>Conectado com sucesso.<h2>";
+    echo "<h2>User '$username' Conectado com sucesso.<h2>";
 } catch(PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
 }
